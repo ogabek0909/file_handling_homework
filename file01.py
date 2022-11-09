03e8ff74bd245b7c89cd315bd5c55a5868a3ca7e
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-09 11:04:06 +0500

    01

--- file01.py
+++ file01.py
@@ -1,4 +1,5 @@
 def main(data:str):
+    
     """
     The data is from the file. Return data as a list type.
     Args:
@@ -6,5 +7,5 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    return open(data).read().split()
 # Read data from file
