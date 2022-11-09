333a79820edbf107976d130caf3bd16e03ff3f48
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-09 11:35:53 +0500

    01

--- file01.py
+++ file01.py
@@ -7,5 +7,7 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    return data.split()
+    return data.split(',')
+
+print (main(open('txt_file/data01.txt').read()))
 # Read data from file
