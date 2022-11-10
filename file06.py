292e2014055abe411d6d3ac0fb0b389764388136
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-10 05:41:55 +0500

    file06.py

--- file06.py
+++ file06.py
@@ -1,4 +1,16 @@
 def main(data:str):
+    array=[]
+    counter=0
+    for i in data:
+        # if i!='\n':
+
+        counter+=1
+
+        if i=='\n':
+            array.append(counter-1)
+            counter=0
+            continue
+
     """
     The data is from the file. Find the each row length and return as list type.
     Args:
@@ -6,5 +18,9 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    return array
+
+print(main("""dsfaf
+dfsafd
+"""))
 # Read data from file
