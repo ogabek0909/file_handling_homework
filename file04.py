d9c9b0f08150ddf783f26b4033dd0d4e4a3182bf
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-10 05:18:18 +0500

    file04.py

--- file04.py
+++ file04.py
@@ -1,4 +1,9 @@
 def main(data:str):
+    array=[]
+    for i in data:
+        if i.isalpha():
+            array.append(i)
+
     """
     The data is from the file. Return the str(non-digital) characters as a list type.
     Args:
@@ -6,5 +11,5 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    return array
 # Read data from file
