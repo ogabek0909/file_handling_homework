de42e0f7a0f836e48332be57f8141f43ee7dba6a
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-10 05:53:37 +0500

    file0

--- file06.py
+++ file06.py
@@ -1,12 +1,14 @@
 def main(data:str):
     array=[]
     counter=0
+    l=0
     for i in data:
         # if i!='\n':
 
         counter+=1
 
         if i=='\n':
+            l+=counter
             array.append(counter-1)
             counter=0
             continue
@@ -18,9 +20,9 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    array.append(len(data)-l)
     return array
 
 print(main("""dsfaf
-dfsafd
-"""))
+dfsafd"""))
 # Read data from file
