341c1d469610ffa3ce26ad67db647ed7e6f0290d
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-10 06:06:16 +0500

    file08.py

--- file08.py
+++ file08.py
@@ -1,4 +1,10 @@
 def main(data:str):
+    
+    s=0
+    for i in data:
+        if i.isdigit():
+            if int(i)>int(s):
+                s=i
     """
     The data is from the file. Find the largest of the numeric characters.
     Args:
@@ -6,5 +12,10 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    return s
+print(main("""Django 4.0
+NumPy 1.22.0
+TensorFlow 2
+python 3.10.0
+Windows 7"""))
 # Read data from file
