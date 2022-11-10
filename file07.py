1d20c64b89adecce6fdd3929ed1a3fa8fd91a5b2
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-10 05:56:51 +0500

    file07.py

--- file07.py
+++ file07.py
@@ -1,4 +1,9 @@
 def main(data:str):
+    s=0
+    for i in data:
+        if i.isdigit():
+            s+=int(i)
+
     """
     The data is from the file. Find a sum of numeric characters and return as list type.
     Args:
@@ -6,5 +11,7 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    return s
 # Read data from file
+print(main("""0707codeschooluz
+python2022"""))
