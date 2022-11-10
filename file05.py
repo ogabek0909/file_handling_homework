8fcbbfb41e6e53586b0a78b091ad42958cd366f1
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-10 05:31:29 +0500

    file05.py

--- file05.py
+++ file05.py
@@ -1,4 +1,14 @@
 def main(data:str):
+    d=0
+    s=0
+    array=[]
+    for i in data:
+        if i.isdigit():
+            d+=1
+        else:
+            s+=1
+    array.append(d)  
+    array.append(s)  
     """
     The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
     Args:
@@ -6,5 +16,6 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    return array
+print(main('fsadfas 342 '))
 # Read data from file
