6c5c86e5464abb4dc2b93fdfc7f240b4f707fb9c
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-10 06:16:45 +0500

    file10.py

--- file10.py
+++ file10.py
@@ -1,4 +1,14 @@
 def main(data:str):
+    array =[]
+    counter=0
+    l=0
+    for i in data:
+        counter +=1
+        if i=='\n':
+            l+=counter
+            array.append(counter-1)
+            counter=0
+    array.append(len(data)-l)
     """
     The data is from the file. Find the each row length and return the largest row.
     Args:
@@ -6,5 +16,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    return max(array)
+print(main("""Computer Vision
+CODESCHOOLUZ
+ML
+CV
+OpenCV
+Numpy
+PYTHON"""))
 # Read data from file
