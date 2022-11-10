7d5a97c11ecf151e9f4731fc81ed2a07c8292159
norpulatovogabek <norpulatovogabek42@gmail.com>
2022-11-10 06:12:04 +0500

    file09.py

--- file09.py
+++ file09.py
@@ -1,4 +1,8 @@
 def main(data:str):
+    array=[]
+    for i in data:
+        if i.isdigit():
+            array.append(i)
     """
     The data is from the file. Find the smallest of the numeric characters.
     Args:
@@ -6,5 +10,10 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    return min(array)
 # Read data from file
+print(main("""Django 4.0
+NumPy 1.22.0
+TensorFlow 2
+python 3.10.0
+Windows 7"""))
